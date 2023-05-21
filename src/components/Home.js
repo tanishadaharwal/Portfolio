b9fd15b62cb6549f13bb459f48d5bb3e66706a39
tanisha <tanisha22daharwal@gmail.com>
2023-05-21 20:51:47 +0530

    added typewriter effect and animated cursor

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,45 @@ import React from "react";
 import girl from "../assets/girl_image.jpg";
 import {CgArrowRight} from "react-icons/cg";
 import {Link} from "react-scroll";
-
+import { useTypewriter } from 'react-simple-typewriter';
+import AnimatedCursor from "react-animated-cursor"
 
 
 const Home = () => {
     
+    const [text] = useTypewriter({
+        words: ['Web Developer', 'Programmar', 'Tech Enthusiast', 'Serial Chiller'],
+        loop: 0
+      })
     
     return (
         <div name="home" className="text-white h-screen w-full bg-gradient-to-b from-black to-gray-800">
-            
+            <AnimatedCursor
+      innerSize={12}
+      outerSize={10}
+      color='236, 72 ,153'
+      outerAlpha={0.5}
+      innerScale={0.3}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
                 
-            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-between h-full px-4 md:flex-row">
-                <div className="flex flex-col justify-center h-full">
-                    <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Web Developer
-                       
-                       
-                    </h2>
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-end h-full px-4 md:flex-row">
+                <div className="flex flex-col w-4/5 justify-center h-full">
+                    <h2 className="pb-4 text-4xl sm:text-7xl font-bold text-white">Hello!</h2>
+                    <h6 className="text-4xl font-bold text-pink-500">I'm a {text}</h6>
                     <p className="text-slate-100 py-4 max-w-md font-semibold">A passionate developer who loves to code cool stuff and solve problems</p>
 
                     <div>
@@ -29,7 +52,7 @@ const Home = () => {
                     </div>
                 </div>
                 <div>
-                    <img src={girl} alt="girl with flowers" className=" hover:scale-105 duration-300 rounded-5xl mx-auto 2-2/3 md:w-full girl"/>
+                    <img src={girl} alt="girl with flowers" className=" hover:scale-105 duration-300 rounded-5xl mx-auto w-2/3 md:w-full girl"/>
                 </div>
             </div>
         </div>
